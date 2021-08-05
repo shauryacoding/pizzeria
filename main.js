53df350fb55cc7ac4187f13eb25f59a3ff12fe89
shauryacoding <82760764+shauryacoding@users.noreply.github.com>
2021-08-05 16:39:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+menu_list_array = ["Veg Margherita Pizza",
+                   "Chicken Tandoori Pizza",
+                   "Veg supreme Pizza",
+                   "Pepperoni Pizza",
+                   "Hawain Pizza",
+                   "Deluxe veggie Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i = 0;i<menu_list_array;i++){
+    htmldata=htmldata '<li>' +menu_list_array[i]+ '<li>'
+}
+htmldata=htmldata+"</ol>";
+document.getElementById("display_menu")
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
