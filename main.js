d045e29f6b6ad29a047d5e2dc87bed7e3761a921
shauryacoding <82760764+shauryacoding@users.noreply.github.com>
2021-08-13 19:18:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var htmldata;
 htmldata="<ol class='menulist'>"
 menu_list_array.sort();
 for(var i = 0;i<menu_list_array;i++){
-    htmldata=htmldata '<li>' +menu_list_array[i]+ '<li>'
+    htmldata=htmldata '<li>' +menu_list_array[i]+ '<li>' 
 }
 htmldata=htmldata+"</ol>";
 document.getElementById("display_menu")
